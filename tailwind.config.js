60526249e00cba1ac4f9198adc81165d749f22e8
saw-koi <97831657+saw-koi@users.noreply.github.com>
2023-03-03 20:18:34 +0900

    Install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+  ],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
