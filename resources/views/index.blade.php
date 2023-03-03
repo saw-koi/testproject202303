9ebab4fe861fb5c417c973259d6ed37ee75d7a79
saw-koi <97831657+saw-koi@users.noreply.github.com>
2023-03-03 22:21:09 +0900

    Use tailwind in blade files

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,13 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body class="antialiased">
-        インデックス
-        <form action="/search">
-            <input type="text" name="q">
-            <input type="submit">
-        </form>
+    <body class="antialiased flex h-screen">
+        <div id="wrapper" class="w-full m-auto">
+            <h1 class="w-full mb-4 text-4xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl"><a href="{{ route('index') }}" class="block">グーグルカスタムサーチAPIをつかってみよう！</a></h1>
+            <form action="/search" class="flex">
+                <input type="text" name="q" placeholder="ここに検索文字列を書いてみよう" class="w-3/4 bg-transparent border border-blue-500 focus:border-blue-300">
+                <input type="submit" value="検索" class="w-1/4 bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded">
+            </form>
+        </div>
     </body>
 </html>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,26 +2,33 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body class="antialiased">
-        <form action="/search">
-            <input type="text" name="q" value="{{ isset($q) ? $q : "" }}">
-            <input type="submit" value="検索">
-        </form>
-        @if( isset($msg) )
-        <p>{{ $msg }}</p>
-        @endif
-        @if( isset($resultCountStr) )
-        <p>{{ $resultCountStr }}件の検索結果</p>
-        @endif
-        @if( isset($items) )
-            @foreach($items as $item)
-            <div class="search-item">
-                <div><a href="{{ $item['link'] }}">{!! $item['htmlTitle'] !!}</a></div>
-                <p>{{ $item['snippet'] }}</p>
-            </div>
-            @endforeach
-        @endif
+        <div id="wrapper" class="w-full">
+            <h1><a href="{{ route('index') }}" class="block text-sm text-center">グーグルカスタムサーチAPIをつかってみよう！</a></h1>
+            <form action="/search" class="flex">
+                <input type="text" name="q" value="{{ isset($q) ? $q : "" }}" placeholder="ここに検索文字列を書いてみよう" class="w-3/4 bg-transparent border border-blue-500 focus:border-blue-300">
+                <input type="submit" value="検索" class="w-1/4 bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded">
+            </form>
+            @if( isset($msg) )
+            <p class="block text-center mx-2 my-2 border-2 border-lime-800 bg-lime-100 rounded-full">{{ $msg }}</p>
+            @endif
+            @if( isset($resultCountStr) )
+            <p class="block text-sm">{{ $resultCountStr }}件の検索結果</p>
+            @endif
+            @if( isset($items) )
+                <ul class="flex grid grid-cols-5 gap-4">
+                    @foreach($items as $item)
+                    <a href="{{ $item['link'] }}" class="col-span-1 border rounded-lg block">
+                        <span class="text-center text-sm block bg-blue-500 text-white rounded-t-lg">{{ $loop->index+1 }}位</span>
+                        <span class="block px-1 text-xl text-center block bg-blue-200">{!! $item['htmlTitle'] !!}</span>
+                        <span class="block text-xs border-b-2 border-gray-300">{!! str_replace('/', '<wbr>/', urldecode($item["link"])) !!}</span>
+                        <span class="text-sm">{{ $item['snippet'] }}</span>
+                    </a>
+                    @endforeach
+                </ul>
+            @endif
+        </div>
     </body>
 </html>
